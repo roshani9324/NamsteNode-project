09a8970cbd69c9630fadac7604ee7523e241b96f
roshanimaurya.in@gmail.com <avasheshmaurya419@gmail.com>
2025-07-20 17:01:16 +0530

    Namaste-React

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom/client"; // ✅ correct import
 const Hello = React.createElement("div", { id: "parent" }, [
   React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "I'm h1 tag "),
+    React.createElement("h1", {}, "This is my First project"),
     React.createElement("h2", {}, "I'm h2 tag "),
   ]),
   React.createElement("div", { id: "child2" }, [
     React.createElement("h1", {}, "I'm h1 tag "),
     React.createElement("h2", {}, "I'm h2 tag "),
+    React.createElement("h3", {}, "I'm h3 tag "),
   ]),
 ]);
 
---
+++ README.md
@@ -0,0 +1,20 @@
+# Namaste React
+
+# Pracel
+-Dev Build
+-Local Server
+-HMR = Hot Module Replacement
+-File Watching Algorithm - written in c++
+-Caching - Faster Builds
+-Image Optimization
+-Minification
+-Bundling
+-Compress
+-Consistent hashing
+-Code Splitting
+-Differential Bundling - support Older browsers
+-Diagnostic
+-Error Handling
+-HTTPs
+-Tree Shaking - remove unused code
+-Different dev and prod bundles
