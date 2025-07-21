e15c3ec7bda35571742c8b40d83bc3f4da23d31a
roshanimaurya.in@gmail.com <avasheshmaurya419@gmail.com>
2025-07-22 01:27:26 +0530

    Namaste React

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client"; // ✅ correct import
+//react,createElement =>It is a Object =>HTMLElement(render) to the browser
 const Hello = React.createElement("div", { id: "parent" }, [
   React.createElement("div", { id: "child" }, [
     React.createElement("h1", {}, "This is my First project"),
@@ -12,5 +13,21 @@ const Hello = React.createElement("div", { id: "parent" }, [
   ]),
 ]);
 
+//React Element 
+const Heading =(
+  <h1 className="head" tabIndex="6">Namsate React using JSX</h1>
+)
+//React functional component or component composition 
+const HeadingComponent = () => (
+  <div id="container">
+    <Heading />
+    <h1>Namsate React Functional Component</h1>
+  </div>
+);
+//JSX - HTML -like or XML- like Syntax
+//JSX is a just Syntax and ReactElement is a Object
+//JSX => Babel transpile it to React.createElement => ReactElement- JS Object => HTMLElement(render)
+//const jsxHeading = <h1>This is using JSX ....</h1>;
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(Hello);
+root.render(<HeadingComponent/>);
